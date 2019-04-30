3237a01b412639907aab09fd55b88f23d628d10e
kurankat <dr.microbe@gmail.com>
2019-05-01 09:54:35 +1000

    Add examples for NewDictWriter and WriteAll

--- example_test.go
+++ example_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/kurankat/csvdict"
@@ -52,4 +53,48 @@ Methuselah,969,Patriarch
 	fmt.Print(records)
 	// Output:
 	// [map[name:Mark Smith  age:33  occupation:Jack of all trades] map[name:Douglas Adams  age:42  occupation:Writer] map[name:Methuselah  age:969  occupation:Patriarch]]
+}
+
+func ExampleDictWriter() {
+	headers := []string{"name", "age", "occupation"}
+	records := []map[string]string{
+		{"name": "Mark Smith", "age": "33", "occupation": "Jack of all trades"},
+		{"name": "Douglas Adams", "age": "42", "occupation": "Writer"},
+		{"name": "Methuselah", "age": "969", "occupation": "Patriarch"},
+	}
+
+	w := csvdict.NewDictWriter(os.Stdout, headers)
+
+	for _, record := range records {
+		err := w.Write(record)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	w.Flush()
+	// Output:
+	// Mark Smith,33,Jack of all trades
+	// Douglas Adams,42,Writer
+	// Methuselah,969,Patriarch
+}
+
+func ExampleDictWriter_WriteAll() {
+	headers := []string{"name", "age", "occupation"}
+	records := []map[string]string{
+		{"name": "Mark Smith", "age": "33", "occupation": "Jack of all trades"},
+		{"name": "Douglas Adams", "age": "42", "occupation": "Writer"},
+		{"name": "Methuselah", "age": "969", "occupation": "Patriarch"},
+	}
+
+	w := csvdict.NewDictWriter(os.Stdout, headers)
+
+	err := w.WriteAll(records)
+	if err != nil {
+		panic(err)
+	}
+	// Output:
+	// Mark Smith,33,Jack of all trades
+	// Douglas Adams,42,Writer
+	// Methuselah,969,Patriarch
 }
